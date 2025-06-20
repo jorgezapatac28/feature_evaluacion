93736f188fb1656366f05aaf2c4315991d6e75a5
jozapata <jzapatca@emea.nttdata.com>
2025-06-20 12:44:31 -0500

    Prueba automatizada karate

--- src/test/java/com/marvel/api/karate/CharacterApiTestRunner.java
+++ src/test/java/com/marvel/api/karate/CharacterApiTestRunner.java
@@ -1,11 +1,16 @@
 package com.marvel.api.karate;
+
 import com.intuit.karate.junit5.Karate;
+
+
 
 public class CharacterApiTestRunner {
     @Karate.Test
     Karate testCharacterApi() {
         return Karate.run("character").relativeTo(getClass());
+
     }
+
     Karate testAll() {
         return Karate.run("com/marvel/api/karate/character.feature").relativeTo(getClass());
     }
